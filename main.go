6229bb14e6efeb318e3b4fdf7d76f8f03e444133
Francisco Riquelme Pérez <francisco.riquelme.perez1999@gmail.com>
2026-07-20 12:40:55 -0400

    update: se creo el endpoind de productos

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+// Paso 1: Definir la estructura del dato (modelo)
+type Producto struct {
+	ID          int     `json:"id"`
+	Nombre      string  `json:"nombre"`
+	Descripcion string  `json:"descripcion"`
+	Precio      float64 `json:"precio"`
+	EnStock     bool    `json:"en_stock"`
+}
+
+// Paso 2: Crear la base de datos simulada en memoria (slice)
+var productos = []Producto{
+	{ID: 1, Nombre: "Producto 1", Descripcion: "Descripción del producto 1", Precio: 19.99, EnStock: true},
+	{ID: 2, Nombre: "Producto 2", Descripcion: "Descripción del producto 2", Precio: 29.99, EnStock: false},
+	{ID: 3, Nombre: "Producto 3", Descripcion: "Descripción del producto 3", Precio: 39.99, EnStock: true},
+}
+
+// Paso 3: Crear el controlador (handler) que procesa la petición y responde json
+func obtenerProductosHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Configurar la cabecera para avisar que enviamos un json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Convertir el slice de productos a json
+	json.NewEncoder(w).Encode(productos)
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "¡Hola! Esta es mi API de Go desplegada de forma 100% automática con CI/CD 🚀")
-	})
+	// Paso 4: Configurar el servidor HTTP y asociarla con su controlador
+	http.HandleFunc("/api/productos", obtenerProductosHandler)
 
+	//Iniciar el servidor en el puerto 8080
 	fmt.Println("Servidor escuchando en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
